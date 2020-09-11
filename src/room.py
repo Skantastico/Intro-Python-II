355634c6195a4b1123962f42daf7ae8be4a4d522
Skantastico <Skantastico@gmail.com>
2020-09-10 22:40:56 -0400

    Completed day 1 mvp

--- src/room.py
+++ src/room.py
@@ -2,4 +2,13 @@
 # description attributes.
 
 class Room(self):
-    pass # initial commit
+    def __init__(self, room_name, description):
+        self.room_name = room_name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return f"{self.room_name}\n {self.description}"
